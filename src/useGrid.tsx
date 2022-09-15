70b33d4a1612259656acd26630f6676b36012ecf
Jorge Rodríguez <39388401+untalchoche@users.noreply.github.com>
2022-09-15 10:28:15 -0500

    initial commit

--- src/useGrid.tsx
+++ src/useGrid.tsx
@@ -1,15 +1,7 @@
-import { useCallback, useReducer } from "react";
+import { useCallback, useReducer, useEffect, useMemo } from "react";
 import { DataViewProps } from "@simpleview/sv-mosaic";
 
-import reducer, { initialState, actions } from "./gridReducer";
-
-const onLimitChange = ({ limit }) => {
-  alert(limit);
-};
-
-const onSkipChange = ({ skip }) => {
-  alert(skip);
-};
+import reducer, { initialState, actions, loadData } from "./gridReducer";
 
 const onSavedViewSave = () => {
   alert("Saving a view is not yet supported");
@@ -31,8 +23,28 @@ export default function useGrid(): DataViewProps {
 
   const onActiveFiltersChange = useCallback(
     ({ activeFilters, filter }) => {
-      console.log({ activeFilters, filter });
       dispatch(actions.activeFilters({ activeFilters, filter }));
+    },
+    [dispatch]
+  );
+
+  const onLimitChange: DataViewProps["onLimitChange"] = useCallback(
+    ({ limit }: { limit: number }) => {
+      dispatch(actions.limit(limit));
+    },
+    [dispatch]
+  );
+
+  const onSkipChange: DataViewProps["onSkipChange"] = useCallback(
+    ({ skip }: { skip: number }) => {
+      dispatch(actions.skip(skip));
+    },
+    [dispatch]
+  );
+
+  const onSortChange: DataViewProps["onSortChange"] = useCallback(
+    (sort) => {
+      dispatch(actions.sort(sort));
     },
     [dispatch]
   );
@@ -41,22 +53,48 @@ export default function useGrid(): DataViewProps {
     return state.views;
   };
 
+  const filters = useMemo(() => {
+    const filters = state.filters.map((val) => {
+      return {
+        name: val.name,
+        label: val.label,
+        type: val.type,
+        args: val.args,
+        component: val.component,
+        onChange: (value) => {
+          dispatch(actions.filter({ name: val.name, value }));
+        }
+      };
+    });
+
+    return filters;
+  }, [state.filters, dispatch]);
+
+  useEffect(() => {
+    loadData(state).then((data) => {
+      dispatch(actions.dataLoaded(data));
+    });
+  }, [state.limit, state.skip, state.sort, state.filter, dispatch]);
+
   return {
     title: "DataView Test",
     count: state.count,
     limit: state.limit,
     skip: state.skip,
+    sort: state.sort,
     buttons: state.buttons,
     columns: state.columns,
-    filters: state.filters,
+    activeColumns: state.activeColumns,
+    filters,
     activeFilters: state.activeFilters,
-    filter: {},
+    filter: state.filter,
     data: state.data,
     primaryActions: state.primaryActions,
     views: state.views,
     savedView: state.savedView,
     onLimitChange,
     onSkipChange,
+    onSortChange,
     onActiveFiltersChange,
     onSavedViewChange,
     onSavedViewSave,
