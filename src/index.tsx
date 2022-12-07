fc6d8896e6aa73a9a01c529efa1745ed85f6f6dd
Owen Allen <oallen@simpleviewinc.com>
2022-12-07 15:48:51 +0000

    Gets closer to working

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from "react";
-import * as ReactDOMClient from "react-dom/client";
+import * as ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
+const root = ReactDOM.createRoot(rootElement!);
 
 root.render(
   <StrictMode>
